1bae1f183a37583e8a669a4cfc2caee8df41bb11
qanuj <a@e10.in>
2014-09-06 22:11:50 +0530

    v0.0.1

---
+++ Vertise/Vertise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vertise.Startup))]
+namespace Vertise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
